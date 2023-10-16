1b2d640a1e2164392270ab8dbf2dc491f8c61310
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2023-10-16 06:19:37 +0000

    add light mode and improve and replace some element with semantic element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
 import reactLogo from "../assets/react.svg";
 export default function Header() {
+  
+  const switchTheme = (e) => {
+    if (e.target.checked) {
+      document.querySelector("body").setAttribute("data-theme", "light");
+    } else {
+      document.querySelector("body").setAttribute("data-theme", "dark");
+    }
+  };
   return (
     <nav className="nav">
       <img src={reactLogo} alt="this is the react logo" className="nav-logo" />
       <h3>ReactFacts</h3>
-      <h4>React course - project 1</h4>
+      <label htmlFor="themeToggle">
+        <input
+          type="checkbox"
+          id="themeToggle"
+          className="theme-toggle-btn"
+          onChange={switchTheme}
+        />
+      </label>
     </nav>
   );
 }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,36 +8,42 @@ export default function MainContent() {
   const addFatcs = () => {
     if (inputValue !== "") {
       setNewFacts([...newFacts, inputValue]);
-      setInputValue('')
+      setInputValue("");
     }
   };
 
   //   setNewFact(false);
   return (
-    <main className="MainContent">
-      <h1 className="MainContent-title">Fun facts about React</h1>
-      <ul className="MainContent-list">
-        <li>Was first released in 2013</li>
-        <li>Was originally created by jordan walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-        {newFacts.map((element, index) => (
-          <li key={index}>{element}</li>
-        ))}
-      </ul>
-      <img src={reactSecondaryLogo} alt="react logo" />
-      <form className="MainContent-form">
-        <input
-          type="text"
-          placeholder="Add new facts"
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-          }}
-        />
-        <input type="button" value="Add Fact" onClick={addFatcs} />
-      </form>
-    </main>
+    <>
+      <main className="MainContent">
+        <h1 className="MainContent-title">Fun facts about React</h1>
+        <ul className="MainContent-list">
+          <li>Was first released in 2013</li>
+          <li>Was originally created by jordan walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintained by facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+          {newFacts.map((element, index) => (
+            <li key={index}>{element}</li>
+          ))}
+        </ul>
+        <aside>
+        <img src={reactSecondaryLogo} alt="react logo" />
+      </aside>
+      </main>
+      <footer>
+        <form className="add-facts">
+          <input
+            type="text"
+            placeholder="Add new facts"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+          <input type="button" value="Add Fact" onClick={addFatcs} />
+        </form>
+      </footer>
+    </>
   );
 }
